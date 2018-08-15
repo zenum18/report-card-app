03402db1faa3f10c6af7b219d26798ba7a9e24cc
Lynnette <lmunez@redventures.com>
2018-08-15 19:02:50 -0400

    added test component

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p> Test String here: {{ testString }}</p>
+    <p> Test Number here: {{ testNumber }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReportCard',
+  props: {
+    testString: String,
+    testNumber: Number,
+  },
+};
+</script>
+
+<style>
+
+</style>
