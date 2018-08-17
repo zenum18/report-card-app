ce09dbee93cbfb9b4663cfd3c8b2e41627352117
Lynnette <lmunez@redventures.com>
2018-08-16 23:30:34 -0400

    added imput and a way to grab it's value and put it in state

---
+++ src/components/MentorReportCardForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form action="">
+    <label for="uName">Username</label>
+    <input
+      type="text"
+      id="uName"
+      name="uName"
+      placeholder="Username"
+      v-model="username"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'MentorReportCardFor',
+  data() {
+    return {
+      username: '',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Test testString="ROAD TO HIRE REPORT CARD" :testNumber=1 />
+    <!-- <Test testString="ROAD TO HIRE REPORT CARD" :testNumber=1 /> -->
+    <MentorReportCardForm  />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-import Test from '@/components/Test.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
+import MentorReportCardForm from '@/components/MentorReportCardForm.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-    Test,
+    MentorReportCardForm,
   },
 };
 </script>
